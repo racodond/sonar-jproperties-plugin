5bda3501e2aed28f8a0facdacf5830bbadbdcf7f
David RACODON <david.racodon@gmail.com>
2016-03-30 15:52:02 +0200

    Update License headers

--- jproperties-checks/src/test/java/org/sonar/jproperties/checks/CommentRegularExpressionCheckTest.java
+++ jproperties-checks/src/test/java/org/sonar/jproperties/checks/CommentRegularExpressionCheckTest.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube Java Properties Plugin
- * Copyright (C) 2015 David RACODON
+ * Copyright (C) 2016 David RACODON
  * david.racodon@gmail.com
  *
  * This program is free software; you can redistribute it and/or
